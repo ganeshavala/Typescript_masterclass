00f09bd2a0c379ec225b96294dae6977538286e3
ganeshavala <gani.143.npt@gmail.com>
2024-02-07 21:33:56 +0530

    TypeScript Masterclass

---
+++ src/ts/services/DataResource.ts
@@ -0,0 +1,32 @@
+export class DataResource<T>{
+constructor(private endpoint:string){
+
+}
+
+async loadAll():Promise<T>{
+    const  res= await fetch(this.endpoint);
+
+    return res.json();
+}
+async loadOne(id:number):Promise<T>{
+    const res= await fetch(`${this.endpoint}/${id}`);
+    return res.json();
+}
+async delete(id:number):Promise<Response>{
+    const res= await fetch(`${this.endpoint}/${id}`,{
+        method:'DELETE'
+    });
+    return res.json();
+}
+async save(data:T):Promise<Response>{
+    const res= await fetch(this.endpoint,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+
+    return res;
+}
+}
